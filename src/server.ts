1dea1298bddc0901cb3f8ba4ec698c926d76b61f
Md Jubair Hossain <mjhshikder.dhaka@gmail.com>
2026-05-16 21:26:14 +0600

    plain text send as broser response

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,23 @@ import { createServer, IncomingMessage, Server, ServerResponse } from "http";
 
 
 const server: Server = createServer((req: IncomingMessage, res: ServerResponse) => {
-    console.log(req.url); 
-    console.log(req.method);
+   // console.log(req.url); //? url ki ki hote pare >>> '/', '/user', '/products'
+   // console.log(req.method); //? method hote pare >>> GET, POST, DELETE, PATCH, PUT
     
+    const url = req.url
+    const method = req.method
+
+    if (url === '/' && method === 'GET') {
+       // console.log("This is Root Route");
+        res.writeHead(200, { 'content-type': 'text/plain' })
+        res.end('This is Root Route')
+    } else {
+         res.writeHead(404, { "content-type": "text/plain" });
+         res.end("Route Not Found");
+    }
+    
+
+
 })
 
 server.listen(5000, () => {
