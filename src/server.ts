a048953fd4d2d675164ce41449bfa0ff1bb80f18
Md Jubair Hossain <mjhshikder.dhaka@gmail.com>
2026-05-16 21:34:20 +0600

    route handling with http request and response

--- src/server.ts
+++ src/server.ts
@@ -10,11 +10,18 @@ const server: Server = createServer((req: IncomingMessage, res: ServerResponse) 
 
     if (url === '/' && method === 'GET') {
        // console.log("This is Root Route");
-        res.writeHead(200, { 'content-type': 'text/plain' })
-        res.end('This is Root Route')
+        res.writeHead(200, { "content-type": "application/json" })
+        res.end(JSON.stringify({ message: "This is Root Route" }));
+        
+
+    } else if (url?.startsWith('/products')) {
+        res.writeHead(200, { "content-type": "application/json" });
+        res.end(JSON.stringify({ message: "This is Products Route" }));
+
+
     } else {
          res.writeHead(404, { "content-type": "text/plain" });
-         res.end("Route Not Found");
+           res.end(JSON.stringify({ message: "Route Not Found" }));
     }
     
 
