6c38247ffdd40fb401205a71a6fae52e99e2c0d1
Md Jubair Hossain <mjhshikder.dhaka@gmail.com>
2026-05-18 15:40:21 +0600

    single product GET function added

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,13 +1,28 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import { readProducts } from "../service/product.service";
+import type { Iproduct } from "../types/product.type";
+import { json } from "stream/consumers";
 
 export const productController = (
   req: IncomingMessage,
   res: ServerResponse,
 ) => {
   const url = req.url;
-  const method = req.method;
+    const method = req.method;
+    
 
+    //? /products => /product/1 => ['', 'products', '1']
+
+
+    const urlParts = url?.split("/")
+   // console.log(urlParts);
+    
+    const id = urlParts && urlParts[1] === 'products' ? Number(urlParts[2]) : null; 
+    
+   // console.log(id);
+    
+
+    //? Get All Products
     if (url === "/products" && method === "GET") {
       
 
@@ -23,12 +38,24 @@ export const productController = (
         // ]
            
         
-        const products = readProducts();
+        const products = readProducts(); // [{},{},{}]
 
     res.writeHead(200, { "content-type": "application/json" });
       res.end(JSON.stringify({
           message: "Products Retrived Successfully",
         data: products
     }));
-  }
+    }
+    //? Single product logic
+    else if (method === "GET" && id !== null) {
+        const allProducts = readProducts();
+        const product = allProducts.find((p: Iproduct) => p.id === id)
+        // console.log(product);
+        
+        res.writeHead(200, { "content-type": "application/json" });
+        res.end(JSON.stringify({
+            message: "Single product retrived successfully",
+            data:product
+        }))
+    }
 };
